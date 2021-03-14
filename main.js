721c6810be852b969dfab3040e3abf09d8eeb488
Terezia Gondova <terezia.gondova@gmail.com>
2021-03-14 16:49:53 +0100

    ukol_01

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+//1.Vytvořte tlačítko, při jehož stisknutí se změní barva Čtvereček na zelenou.
+let squareElement = document.querySelector(".ctverecek");
 
-/**
- * Sečte dvě čísla na vstupu a vrátí výsledek
- *
- * @param {int} a
- * @param {int} b
- */
+function changeColorTo () {
+  squareElement.style.backgroundColor="green";
+}
+
+//2.Upravte kód tak, aby se výsledek funkce secti() zobrazil v prvku s ID vysledek až po stiknutí nově přidaného tlačítka "Zobraz výsledek".
+let calculation = document.querySelector("#vysledek").innerHTML;
+
+function showRsesult(){
+  calculation.valueOf = secti(4,5);
+}
+
 function secti(a, b) {
   let c = a + b;
   return c;
 }
 
-/**
- * Upozorní uživatele při spuštění.
- */
+
+ //3.Uprav funkci upozorni() tak, aby se navíc vypisoval text i do konzole a také nahradil text v elementu Čtvereček.
+ 
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+  squareElement.innerHTML ="Gratulace, právě jsi spustila tuto funkci!";
 }
+
+
+
